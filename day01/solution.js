eff80a8463790ec957cea31294ef7c328f21dca2
ljwedin <ljwedin@gmail.com>
2023-12-05 15:47:50 +0100

    solution day one

--- day01/solution.js
+++ day01/solution.js
@@ -1,4 +1,86 @@
-const testString = 'sdfko1lk2lkj3nl4s';
+const { log } = require('console');
+const { readFileSync } = require('fs');
+const { text } = require('stream/consumers');
+const inputLines = readFile('input.txt');
+
+function readFile(filename) {
+    const contents = readFileSync(`${__dirname}/${filename}`, 'utf-8');
+    const arr = contents.split(/\r?\n/);
+
+    return arr;
+}
+
+function partOne() {
+    let sum = 0;
+
+    for (let line of inputLines) {
+        let firstDigit, lastDigit;
+
+        for (let char of line) {
+            if (isCharDigit(char)) {
+                if (!firstDigit) {
+                    firstDigit = char;
+                } else {
+                    lastDigit = char;
+                }
+            }
+        }
+
+        if (!lastDigit) {
+            lastDigit = firstDigit;
+        }
+
+        //console.log('Last digit: ' + lastDigit)
+
+        const number = parseInt(firstDigit + lastDigit);
+        sum += number;
+    }
+
+    return sum;
+}
+
+function partTwo() {
+    const textDigits = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    ];
+
+    let sum = 0;
+
+    for (let line of inputLines) {
+        let digits = [];
+        for (let i = 0; i < line.length; i++) {
+            const char = line[i];
+
+            if (isCharDigit(char)) {
+                digits.push(char);
+            }
+
+            const lineSubstring = line.substring(i);
+
+            for (let j = 0; j < textDigits.length; j++) {
+                const textDigit = textDigits[j];
+
+                if (lineSubstring.startsWith(textDigit)) {
+                    digits.push(j + 1);
+                }
+            }
+        }
+        const lastIndex = digits.length - 1;
+        const number = `${digits[0]}${digits[lastIndex]}`;
+
+        console.log(number);
+        sum += Number(number);
+    }
+    console.log(sum);
+}
 
 function isCharDigit(char) {
     if (char.match(/[1-9]/g)) {
@@ -8,9 +90,10 @@ function isCharDigit(char) {
     }
 }
 
-function returnDigits(inputString) {
-    const mockResult = ['1', '2', '3', '4'];
-    return mockResult;
-}
+partTwo();
 
-module.exports = { isCharDigit, returnDigits };
+module.exports = {
+    partOne,
+    partTwo,
+    isCharDigit,
+};
--- day01/solution.test.js
+++ day01/solution.test.js
@@ -1,14 +1,17 @@
-const { isCharDigit, returnDigits } = require('./solution');
+const { partOne, partTwo, isCharDigit } = require('./solution');
 
 test("Returns true with input '1'", () => {
     expect(isCharDigit('1')).toBe(true);
 });
 
-test("Returns false with input 'a'"),
-    () => {
-        expect(isCharDigit('a')).toBe(false);
-    };
+test("Returns false with input 'a'", () => {
+    expect(isCharDigit('a')).toBe(false);
+});
 
-test("Returns the array ['1', '2',  '3', '4'] from the string 'sdfko1lk2lkj3nl4s'", () => {
-    expect(returnDigits('sdfko1lk2lkj3nl4s')).toEqual(['1', '2', '3', '4']);
+test('Returns the sum 55447', () => {
+    expect(partOne()).toEqual(55447);
+});
+
+test('Returns the sum 30', () => {
+    expect(partTwo()).toEqual(30);
 });
