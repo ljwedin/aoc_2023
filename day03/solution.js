334e6186c5b32ded98a63a2031b1b96cb75ff3aa
Lisa <ljwedin@gmail.com>
2023-12-08 13:47:33 +0100

    halfway through day 3 part two

--- day03/solution.js
+++ day03/solution.js
@@ -1,4 +1,5 @@
 const { readFileSync } = require('fs');
+const { availableParallelism } = require('os');
 const inputLines = readFile('input.txt');
 
 function readFile(filename) {
@@ -37,12 +38,6 @@ function extractDigits(inputString, lineIndex) {
 
     for (let i = 0; i < inputString.length; i++) {
         const char = inputString[i];
-        if (i === 28 && lineIndex === 137) {
-            console.log('28 char: ', char);
-        }
-        if (i === 29 && lineIndex === 137) {
-            console.log('29 char is digit: ', isDigit(char));
-        }
 
         if (isDigit(char)) {
             if (currentDigits === '') {
@@ -154,4 +149,158 @@ function partOne() {
     return sum;
 }
 
-console.log(partOne());
+function isGear(char) {
+    if (char === '*') {
+        return true;
+    }
+
+    return false;
+}
+
+function extractGears() {
+    const gears = [];
+
+    for (let i = 0; i < inputLines.length; i++) {
+        const thisLine = inputLines[i];
+
+        for (let j = 0; j < thisLine.length; j++) {
+            let char = thisLine[j];
+
+            if (isGear(char)) {
+                gears.push({
+                    lineIndex: i,
+                    charIndex: j,
+                });
+            }
+        }
+    }
+
+    return gears;
+}
+
+function isValidGear(gear) {
+    let adjacentNumberAmount = 0;
+    const thisLine = inputLines[gear.lineIndex];
+    const previousLineSlice = inputLines[gear.lineIndex - 1].slice(
+        gear.charIndex - 1,
+        gear.charIndex + 2
+    );
+    const nextLineSlice = inputLines[gear.lineIndex + 1].slice(
+        gear.charIndex - 1,
+        gear.charIndex + 2
+    );
+    let adjacentNumberIndexes = [];
+
+    if (gear.charIndex > 0 && isDigit(thisLine[gear.charIndex - 1])) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex,
+            index: gear.charIndex - 1,
+        });
+    }
+
+    if (
+        gear.charIndex < thisLine.length - 1 &&
+        isDigit(thisLine[gear.charIndex + 1])
+    ) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex,
+            index: gear.charIndex + 1,
+        });
+    }
+
+    if (isDigit(previousLineSlice[0])) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex - 1,
+            index: gear.charIndex - 1,
+        });
+    }
+
+    if (
+        isDigit(previousLineSlice[0]) &&
+        !isDigit(previousLineSlice[1]) &&
+        isDigit(previousLineSlice[2])
+    ) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex - 1,
+            index: gear.charIndex + 1,
+        });
+    }
+
+    if (isDigit(nextLineSlice[0])) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex + 1,
+            index: gear.charIndex + 1,
+        });
+    }
+
+    if (
+        isDigit(nextLineSlice[0]) &&
+        !isDigit(nextLineSlice[1]) &&
+        isDigit(nextLineSlice[2])
+    ) {
+        adjacentNumberAmount++;
+        adjacentNumberIndexes.push({
+            line: gear.lineIndex + 1,
+            index: gear.charIndex + 1,
+        });
+    }
+
+    if (adjacentNumberAmount == 2) {
+        return adjacentNumberIndexes;
+    }
+
+    return false;
+}
+
+function findWholeNumber(gear) {
+    const thisLine = inputLines[gear.line];
+    console.log(`Char index: ${gear.index}`);
+    const startChar = thisLine[gear.index];
+    const startCharIndex = gear.index;
+    let number = [startChar];
+
+    for (let i = startCharIndex; i < thisLine.length; i++) {
+        if (!isDigit(thisLine[i])) {
+            number.push(thisLine[i - 1]);
+            break;
+        }
+    }
+
+    for (let i = startCharIndex; i > 0; i--) {
+        if (!isDigit(thisLine[i])) {
+            number.unshift(thisLine[i + 1]);
+            break;
+        }
+    }
+
+    console.log(number);
+}
+
+function partTwo() {
+    const gears = extractGears();
+    const result = [];
+    let sum = 0;
+
+    for (let gear of gears) {
+        const validGear = isValidGear(gear);
+        if (validGear) {
+            const firstNumber = findWholeNumber(validGear[0]);
+            const secondNumber = findWholeNumber(validGear[1]);
+
+            result.push(firstNumber * secondNumber);
+        }
+    }
+
+    for (let number in result) {
+        sum += number;
+    }
+
+    return gears;
+}
+
+partTwo();
