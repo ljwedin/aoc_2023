bfebebc7e9073e723921687402778239acaa617c
Lisa <ljwedin@gmail.com>
2023-12-10 16:01:36 +0100

    used others solution because of insanity reasons

--- day03/solution.js
+++ day03/solution.js
@@ -1,306 +1,85 @@
-const { readFileSync } = require('fs');
-const { availableParallelism } = require('os');
-const inputLines = readFile('input.txt');
+let total = 0;
+let secondTotal = 0;
+let finalGearNumbers = [];
 
-function readFile(filename) {
-    const contents = readFileSync(`${__dirname}/${filename}`, 'utf-8');
-    const arr = contents.split(/\r?\n/);
+const fs = require('fs');
+const text = fs.readFileSync('./input.txt', 'utf-8');
+const textByLine = text.split(/\r\n|(?<!\r)\n|\r/);
 
-    return arr;
-}
+textByLine.forEach((line, lineIndex) => {
+    let number = '';
+    let startIndex = 0;
 
-function isDigit(char) {
-    if (char.match(/[0-9]/g)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function isSymbol(char) {
-    if (!isDigit(char)) {
-        if (char !== '.') {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function extractDigits(inputString, lineIndex) {
-    const result = [];
-    let sum = 0;
-    let currentDigits = '';
-    let startingIndex = null;
-    let endingIndex = null;
-    const isFirstLine = lineIndex === 0 ? true : false;
-    const isLastLine = lineIndex === inputLines.length - 1 ? true : false;
-
-    for (let i = 0; i < inputString.length; i++) {
-        const char = inputString[i];
-
-        if (isDigit(char)) {
-            if (currentDigits === '') {
-                startingIndex = i;
+    for (let i = 0; i <= line.length; i++) {
+        if (/\d/.test(line[i])) {
+            if (number === '') {
+                startIndex = i;
             }
-            currentDigits += char;
+            number += line[i];
         } else {
-            if (currentDigits.length > 0) {
-                endingIndex = i - 1;
-                if (
-                    isValidEnginePart(
-                        lineIndex,
-                        startingIndex,
-                        endingIndex,
-                        isFirstLine,
-                        isLastLine
-                    )
-                ) {
-                    result.push(parseInt(currentDigits));
+            if (number !== '') {
+                let valid = false;
+
+                for (let j = lineIndex - 1; j <= lineIndex + 1; j++) {
+                    if (textByLine[j] !== undefined) {
+                        for (
+                            let k = startIndex - 1;
+                            k <= startIndex + number.length;
+                            k++
+                        ) {
+                            if (textByLine[j][k] !== undefined) {
+                                if (/[^0-9.]/.test(textByLine[j][k])) {
+                                    valid = true;
+                                }
+                            }
+                        }
+                    }
                 }
-                currentDigits = '';
+
+                if (valid) {
+                    total += parseInt(number);
+                }
+            }
+            number = '';
+        }
+    }
+});
+
+textByLine.forEach((line, lineIndex) => {
+    for (let i = 0; i <= line.length; i++) {
+        if (line[i] === '*') {
+            let numbers = [];
+
+            for (let j = lineIndex - 1; j <= lineIndex + 1; j++) {
+                if (textByLine[j] !== undefined) {
+                    for (let k = i - 1; k <= i + 1; k++) {
+                        if (textByLine[j][k] !== undefined) {
+                            if (/\d/.test(textByLine[j][k])) {
+                                let number = '';
+                                while (/\d/.test(textByLine[j][k])) {
+                                    k--;
+                                }
+                                k++;
+                                while (/\d/.test(textByLine[j][k])) {
+                                    number += textByLine[j][k];
+                                    k++;
+                                }
+                                numbers.push(parseInt(number));
+                            }
+                        }
+                    }
+                }
+            }
+            if (numbers.length === 2) {
+                finalGearNumbers.push(numbers[0] * numbers[1]);
             }
         }
     }
+});
 
-    if (currentDigits.length > 0) {
-        endingIndex = inputString.length - 1;
-        startingIndex = endingIndex - (currentDigits.length - 1);
+finalGearNumbers.forEach((number) => {
+    secondTotal += number;
+});
 
-        if (
-            isValidEnginePart(
-                lineIndex,
-                startingIndex,
-                endingIndex,
-                isFirstLine,
-                isLastLine
-            )
-        ) {
-            result.push(parseInt(currentDigits));
-        }
-        currentDigits = '';
-    }
-
-    for (let number of result) {
-        sum += number;
-    }
-
-    return sum;
-}
-
-function isValidEnginePart(
-    lineIndex,
-    numberStartIndex,
-    numberStopIndex,
-    isFirstLine,
-    isLastLine
-) {
-    let thisLine = inputLines[lineIndex];
-    const startIndexExtended = numberStartIndex == 0 ? 0 : numberStartIndex - 1;
-    const stopIndexExtended =
-        numberStopIndex == thisLine.length - 1
-            ? thisLine.length - 1
-            : numberStopIndex + 1;
-    const previousLineIndex = isFirstLine ? 0 : lineIndex - 1;
-    const nextLineIndex = isLastLine ? inputLines.length - 1 : lineIndex + 1;
-    const previousLineSlice = inputLines[previousLineIndex].slice(
-        startIndexExtended,
-        stopIndexExtended + 1
-    );
-    const nextLineSlice = inputLines[nextLineIndex].slice(
-        startIndexExtended,
-        stopIndexExtended + 1
-    );
-
-    if (
-        isSymbol(thisLine[startIndexExtended]) ||
-        isSymbol(thisLine[stopIndexExtended])
-    ) {
-        return true;
-    }
-
-    for (let char of previousLineSlice) {
-        if (isSymbol(char)) {
-            return true;
-        }
-    }
-
-    for (let char of nextLineSlice) {
-        if (isSymbol(char)) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-function partOne() {
-    const result = [];
-    let sum = 0;
-
-    for (i = 0; i < inputLines.length; i++) {
-        result.push(extractDigits(inputLines[i], i));
-    }
-
-    for (let number of result) {
-        sum += number;
-    }
-
-    return sum;
-}
-
-function isGear(char) {
-    if (char === '*') {
-        return true;
-    }
-
-    return false;
-}
-
-function extractGears() {
-    const gears = [];
-
-    for (let i = 0; i < inputLines.length; i++) {
-        const thisLine = inputLines[i];
-
-        for (let j = 0; j < thisLine.length; j++) {
-            let char = thisLine[j];
-
-            if (isGear(char)) {
-                gears.push({
-                    lineIndex: i,
-                    charIndex: j,
-                });
-            }
-        }
-    }
-
-    return gears;
-}
-
-function isValidGear(gear) {
-    let adjacentNumberAmount = 0;
-    const thisLine = inputLines[gear.lineIndex];
-    const previousLineSlice = inputLines[gear.lineIndex - 1].slice(
-        gear.charIndex - 1,
-        gear.charIndex + 2
-    );
-    const nextLineSlice = inputLines[gear.lineIndex + 1].slice(
-        gear.charIndex - 1,
-        gear.charIndex + 2
-    );
-    let adjacentNumberIndexes = [];
-
-    if (gear.charIndex > 0 && isDigit(thisLine[gear.charIndex - 1])) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex,
-            index: gear.charIndex - 1,
-        });
-    }
-
-    if (
-        gear.charIndex < thisLine.length - 1 &&
-        isDigit(thisLine[gear.charIndex + 1])
-    ) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex,
-            index: gear.charIndex + 1,
-        });
-    }
-
-    if (isDigit(previousLineSlice[0])) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex - 1,
-            index: gear.charIndex - 1,
-        });
-    }
-
-    if (
-        isDigit(previousLineSlice[0]) &&
-        !isDigit(previousLineSlice[1]) &&
-        isDigit(previousLineSlice[2])
-    ) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex - 1,
-            index: gear.charIndex + 1,
-        });
-    }
-
-    if (isDigit(nextLineSlice[0])) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex + 1,
-            index: gear.charIndex + 1,
-        });
-    }
-
-    if (
-        isDigit(nextLineSlice[0]) &&
-        !isDigit(nextLineSlice[1]) &&
-        isDigit(nextLineSlice[2])
-    ) {
-        adjacentNumberAmount++;
-        adjacentNumberIndexes.push({
-            line: gear.lineIndex + 1,
-            index: gear.charIndex + 1,
-        });
-    }
-
-    if (adjacentNumberAmount == 2) {
-        return adjacentNumberIndexes;
-    }
-
-    return false;
-}
-
-function findWholeNumber(gear) {
-    const thisLine = inputLines[gear.line];
-    console.log(`Char index: ${gear.index}`);
-    const startChar = thisLine[gear.index];
-    const startCharIndex = gear.index;
-    let number = [startChar];
-
-    for (let i = startCharIndex; i < thisLine.length; i++) {
-        if (!isDigit(thisLine[i])) {
-            number.push(thisLine[i - 1]);
-            break;
-        }
-    }
-
-    for (let i = startCharIndex; i > 0; i--) {
-        if (!isDigit(thisLine[i])) {
-            number.unshift(thisLine[i + 1]);
-            break;
-        }
-    }
-
-    console.log(number);
-}
-
-function partTwo() {
-    const gears = extractGears();
-    const result = [];
-    let sum = 0;
-
-    for (let gear of gears) {
-        const validGear = isValidGear(gear);
-        if (validGear) {
-            const firstNumber = findWholeNumber(validGear[0]);
-            const secondNumber = findWholeNumber(validGear[1]);
-
-            result.push(firstNumber * secondNumber);
-        }
-    }
-
-    for (let number in result) {
-        sum += number;
-    }
-
-    return gears;
-}
-
-partTwo();
+console.log(`First solution: ${total}`);
+console.log(`Second solution: ${secondTotal}`);
